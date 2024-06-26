91b8afd70df8ab1d238ec8faa676ecb4c0eed297
rarc88 <rarc88@gmail.com>
2024-06-26 08:10:29 -0500

    Checkpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { Elysia } from "elysia";
-import { PrismaClient } from "@prisma/client";
+import {Elysia} from 'elysia';
+import {PrismaClient} from '@prisma/client';
 
-const prisma = new PrismaClient();
+new PrismaClient();
 const app = new Elysia();
 
-app.get("/", () => "Hello Elysia")
+app.get('/', () => 'Hello Elysia');
 app.listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
